12e0101e48e012722970054eeb5e7cf4e5c876a8
J S <smutek.jakub@seznam.cz>
2024-11-28 18:40:29 +0100

    added glad

--- src/Window.cpp
+++ src/Window.cpp
@@ -2,14 +2,28 @@
 
 #include <iostream>
 
-
-//! \brief constructs SDL window using it's \p width and \p height 
+//! \brief constructs SDL window using it's \p width and \p height
 //! \param width
 //! \param height
 Window::Window(int width, int height)
     : RenderTarget(width, height)
 {
+    SDL_Init(SDL_INIT_VIDEO);
 
+// Create OpenGL context on SDL window
+#ifdef __EMSCRIPTEN__ //! emscripten does it on its own
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#else
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+#endif
+
+    SDL_GL_SetSwapInterval(1);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     // Create SDL window
     m_handle =
         SDL_CreateWindow("Hello Triangle Minimal",
@@ -17,27 +31,25 @@ Window::Window(int width, int height)
                          width, height,
                          SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN);
 
-    // Create OpenGL context on SDL window
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    SDL_GL_SetSwapInterval(1);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     m_gl_context = SDL_GL_CreateContext(m_handle);
+
+//! load gl functions on desktops (emscripten does it on it's own)
+#ifndef __EMSCRIPTEN__
+    int version = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
+#endif
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     // glEnable(GL_DEPTH_TEST);
     // glDepthFunc(GL_LESS);
     glCheckError();
     printf("INFO: GL version: %s\n", glGetString(GL_VERSION));
-    
+
     // Get actual GL window size in pixels, in case of high dpi scaling
     utils::Vector2i size_check;
     SDL_GL_GetDrawableSize(m_handle, &size_check.x, &size_check.y);
     printf("INFO: GL window size = %dx%d\n", size_check.x, size_check.y);
     printf("INFO: Desired Window size = %dx%d\n", width, height);
- 
+
     glViewport(0, 0, size_check.x, size_check.y);
 }
 
@@ -59,7 +71,6 @@ SDL_Window *Window::getHandle() const
     return m_handle;
 }
 
-
 //! \returns an associated SDL_GLcontext
 SDL_GLContext *Window::getContext()
 {
@@ -74,7 +85,7 @@ void Window::setSize(int width, int height)
 void Window::onResize()
 {
     SDL_GetWindowSize(m_handle, &m_target_size.x, &m_target_size.y);
-    glViewport(0,0,m_target_size.x, m_target_size.y);
+    glViewport(0, 0, m_target_size.x, m_target_size.y);
 }
 
 //! \brief closes the window
