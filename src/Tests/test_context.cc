cc10d8506b4fe6095f6535498ead73a6bd708655
J S <smutek.jakub@seznam.cz>
2024-12-04 18:20:05 +0100

    added function laod tests

--- src/Tests/test_context.cc
+++ src/Tests/test_context.cc
@@ -3,7 +3,7 @@
 #include <Window.h>
 #include "IncludesGl.h"
 
-TEST(TestContext, FunctionLoad)
+TEST(TestContext, GladFunctionLoad)
 {
     int width = 800;
     int height = 600;
@@ -57,6 +57,40 @@ TEST(TestContext, VersionCreation)
 
     EXPECT_TRUE(major_version == 4);
     EXPECT_TRUE(minor_version == 6);
+
+    SDL_GL_DeleteContext(m_gl_context);
+    SDL_DestroyWindow(m_handle);
+    SDL_Quit();
+}
+
+TEST(TestContext, GLFunctionLoad)
+{
+    int width = 800;
+    int height = 600;
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+
+    // Create SDL window
+    SDL_Window *m_handle =
+        SDL_CreateWindow("Test",
+                         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                         width, height,
+                         SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN);
+
+    auto m_gl_context = SDL_GL_CreateContext(m_handle);
+
+    int version = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
+
+    EXPECT_TRUE(glGetString != nullptr);
+    EXPECT_TRUE(glCreateBuffers != nullptr);
+    EXPECT_TRUE(glBindBuffer != nullptr);
+    EXPECT_TRUE(glTexImage2D != nullptr);
+    EXPECT_TRUE(glDrawArrays != nullptr);
+    EXPECT_TRUE(glDrawArraysInstanced != nullptr);
+    EXPECT_TRUE(glCreateProgram != nullptr);
+    EXPECT_TRUE(glLinkProgram != nullptr);
 
     SDL_GL_DeleteContext(m_gl_context);
     SDL_DestroyWindow(m_handle);
