b8bfcd038dd8aa8fcc7f28e4c7b139e74e7298f8
J S <smutek.jakub@seznam.cz>
2024-12-04 18:07:58 +0100

    glad load test

--- src/Tests/test_context.cc
+++ src/Tests/test_context.cc
@@ -3,17 +3,17 @@
 #include <Window.h>
 #include "IncludesGl.h"
 
-TEST(TestContext, VersionCreation)
+TEST(TestContext, FunctionLoad)
 {
-    int width = 800; 
-    int height = 600; 
+    int width = 800;
+    int height = 600;
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
 
     // Create SDL window
-    SDL_Window* m_handle =
+    SDL_Window *m_handle =
         SDL_CreateWindow("Test",
                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                          width, height,
@@ -23,31 +23,24 @@ TEST(TestContext, VersionCreation)
 
     int version = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
 
-    int major_version;
-    int minor_version;
-    glGetIntegerv(GL_MAJOR_VERSION, &major_version);
-    glGetIntegerv(GL_MINOR_VERSION, &minor_version);
-
-    EXPECT_TRUE(major_version == 4);
-    EXPECT_TRUE(minor_version == 6);
+    EXPECT_TRUE(version != 0);
 
     SDL_GL_DeleteContext(m_gl_context);
     SDL_DestroyWindow(m_handle);
     SDL_Quit();
 }
 
-
 TEST(TestContext, VersionCreation)
 {
-    int width = 800; 
-    int height = 600; 
+    int width = 800;
+    int height = 600;
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
 
     // Create SDL window
-    SDL_Window* m_handle =
+    SDL_Window *m_handle =
         SDL_CreateWindow("Test",
                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                          width, height,
