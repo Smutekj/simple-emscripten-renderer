826a15da8288ca6960db355ab0b28f584d30410c
Smutekj <smutek.jakub@seznam.cz>
2024-12-13 09:12:38 +0100

    wrong include

--- include/Utils/RandomTools.h
+++ include/Utils/RandomTools.h
@@ -2,15 +2,15 @@
 
 #include <random>
 
-#include "../Vertex.h"
+#include "Vector2.h"
 
 inline float randf(const float min = 0, const float max = 1)
 {
     return (rand() / static_cast<float>(RAND_MAX)) * (max - min) + min;
 }
 
-inline Vec2 randomPosInBox(Vec2 ul_corner,
-                            Vec2 box_size)
+inline Vec2 randomPosInBox(utils::Vector2f ul_corner,
+                           utils::Vector2f box_size)
 {
     return {ul_corner.x + rand() / static_cast<float>(RAND_MAX) * box_size.x,
             ul_corner.y + rand() / static_cast<float>(RAND_MAX) * box_size.y};
