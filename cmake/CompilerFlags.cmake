5b34d8de1f5d8ab54adc93285967a8c1e8241a87
J S <smutek.jakub@seznam.cz>
2024-12-19 08:59:51 +0100

    fixed flag warnings

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -12,14 +12,16 @@ add_compile_options(  $<$<CONFIG:Release>:-march=native>)
 endif()
 
 
-
 if( ${CMAKE_SYSTEM_NAME} MATCHES "Emscripten")
     set(USE_FLAGS "-s USE_SDL=2 -s FULL_ES3=1 -s OFFSCREEN_FRAMEBUFFER=1")
     set(USE_FLAGS "${USE_FLAGS} -s MIN_WEBGL_VERSION=1 -s MAX_WEBGL_VERSION=2")
     set(USE_FLAGS "${USE_FLAGS} -s ASSERTIONS=0 -s ALLOW_MEMORY_GROWTH=1 -s ASYNCIFY=1")
 
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${USE_FLAGS} --profiling --embed-file ${CMAKE_SOURCE_DIR}/Resources --embed-file ${CMAKE_SOURCE_DIR}/external/lygia" )
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${USE_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -s USE_SDL=2 --profiling" )
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${USE_FLAGS} \
+                                                --embed-file ${CMAKE_SOURCE_DIR}/external/lygia \
+                                                --embed-file ${CMAKE_SOURCE_DIR}/Resources")
+    # target_link_options(renderer PUBLIC )
     set(CMAKE_EXECUTABLE_SUFFIX .html)
 endif()
 
