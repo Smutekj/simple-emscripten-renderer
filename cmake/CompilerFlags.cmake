7c85b61af0e954f80c5333f3e5327da0d34445ff
J S <smutek.jakub@seznam.cz>
2025-07-06 07:45:47 +0200

    trying fixes

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -9,18 +9,21 @@ function(set_target_compiler_flags target_name)
   if(CMAKE_SYSTEM_NAME STREQUAL "Emscripten")
     target_compile_options(${target_name} PUBLIC
     "-sUSE_SDL=2"
+    "-sMIN_WEBGL_VERSION=2"
+    "-sMAX_WEBGL_VERSION=2"
     )
     
     target_link_options(${target_name} PUBLIC
-    "-s USE_SDL=2"
-    "-s FULL_ES3=1"
-    "-s OFFSCREEN_FRAMEBUFFER=1"
-    "-s MIN_WEBGL_VERSION=1"
-    "-s MAX_WEBGL_VERSION=2"
-    "-s ASSERTIONS=1"
-    "-s ALLOW_MEMORY_GROWTH=1"
-    "-s ASYNCIFY=1"
-    "-s EXCEPTION_CATCHING_ALLOWED=yes" 
+    "-sUSE_SDL=2"
+    "-sFULL_ES3=1"
+    "-sUSE_WEBGL2=1"
+    "-sOFFSCREEN_FRAMEBUFFER=1"
+    "-sMIN_WEBGL_VERSION=2"
+    "-sMAX_WEBGL_VERSION=2"
+    "-sASSERTIONS=1"
+    "-sALLOW_MEMORY_GROWTH=1"
+    "-sASYNCIFY=1"
+    "-sEXCEPTION_CATCHING_ALLOWED=yes" 
     )
   endif()
 
