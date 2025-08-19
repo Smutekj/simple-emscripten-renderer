6273ee9c04d22cb6b3c2c5276a3183e794f9dda7
Smutekj <smutek.jakub@seznam.cz>
2025-08-19 14:23:32 +0200

    cosnt correct text

--- include/Renderer.h
+++ include/Renderer.h
@@ -56,7 +56,7 @@ public:
 
     void drawSprite(Sprite &sprite, const std::string &shader_id = "SpriteDefault", DrawType draw_type = DrawType::Dynamic);
     void drawSpriteDynamic(Sprite &sprite, const std::string &shader_id);
-    void drawText(Text &text, const std::string &shader_id = "TextDefault", DrawType draw_type = DrawType::Dynamic);
+    void drawText(const Text &text, const std::string &shader_id = "TextDefault", DrawType draw_type = DrawType::Dynamic);
     void drawLine(Vec2 point_a, Vec2 point_b, float thickness, Color color);
     void drawRectangle(RectangleSimple &r, Color color, const std::string &shader_id = "VertexArrayDefault", DrawType draw_type = DrawType::Dynamic);
     void drawLineBatched(Vec2 point_a, Vec2 point_b, float thickness, Color color, DrawType draw_type = DrawType::Dynamic);
